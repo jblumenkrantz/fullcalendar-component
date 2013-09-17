997bc4269e7abde8297d192bd9d955c8b7c1b04b
evan <evan@sdicg.com>
2013-09-17 15:41:02 -0400

    adding tasks with due dates to event list

--- www/modules/calendar/calendarCtl.js
+++ www/modules/calendar/calendarCtl.js
@@ -1,18 +1,23 @@
 'use strict';
 
 angular.module('pinwheelApp')
-  .controller('CalendarCtl', function ($scope, $routeParams, $location, Calendar, Event, Task) {
+  .controller('CalendarCtl', function ($filter, $scope, $routeParams, $location, Calendar, Event, Task) {
 
 		$scope.view = "list";
 		$scope.calendarWatchers = {};
 
 		Calendar.query({id: 'all'}, function(calendars){
 			$scope.calendars = calendars;
-			Task.query({id: 'all'}, function(tasks){
-				$scope.tasks = tasks;
-			});
 			Event.query({id: 'all'}, function(events){
 				$scope.events = events;
+				Task.query({id: 'all'}, function(tasks){
+					$scope.tasks = tasks;
+					angular.forEach($scope.tasks, function(task){
+						if(task.due_time){
+							$scope.events.push(task);
+						}
+					});
+				});
 			});
 		}, function(error){
 			// TODO: update this and other requests
@@ -20,10 +25,6 @@ angular.module('pinwheelApp')
 			$scope.logout();
 		});
 		
-		Event.query({id: 'all'}, function(events){
-			$scope.events = events;
-		});
-
 		$scope.isCalendarShowing = function(item) {
 			return $scope.calendarWatchers[item.calendar_id] && $scope.calendarWatchers[item.calendar_id].viewing;
 		}
