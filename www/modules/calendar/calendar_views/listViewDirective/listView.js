cb9c627919d4ac96df57c9b0e98af66ab5c8e51c
Gabriel Masters-Jackson <gabriel.mj2@gmail.com>
2013-09-18 11:40:51 -0400

    adding/editing events minus api calls

--- www/modules/calendar/calendar_views/listViewDirective/listView.js
+++ www/modules/calendar/calendar_views/listViewDirective/listView.js
@@ -5,14 +5,7 @@ angular.module('pinwheelApp')
 		return {
 			restrict: 'E',
 			templateUrl: 'modules/calendar/calendar_views/listViewDirective/_list_view.html',
-			scope: {
-				task: '=',
-				calendars: '=',
-				events: '=',
-				colors: '=',
-				filter: '&'
-			},
-			controller: function($scope, $element, $attrs, $routeParams){
+			controller: function($scope) {
 			}
 		}
 	});
