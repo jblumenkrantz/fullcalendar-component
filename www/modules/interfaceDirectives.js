276ed28c606f6607fa6ad0fb71ed4a5452eef44d
Gabriel Masters-Jackson <gabriel.mj2@gmail.com>
2013-09-24 15:30:49 -0400

    calendar sidebar scrolling works, better drawer behavior

--- www/modules/interfaceDirectives.js
+++ www/modules/interfaceDirectives.js
@@ -1,24 +1,30 @@
 'use strict';
 
 angular.module('pinwheelApp')
-.directive('scrollPane', function(Debounce) {
-	return function(scope, element, attrs) {
-		function getHeight() {
-			return $(window).height() - $("#mainHeader").height() - element.siblings(".scroll-header").height() + "px";
-		}
-
-		if ($(window).width() > 768) {
-			element.css("height", getHeight());
-		}
-
-		$(window).resize(Debounce(function() {
-			if ($(window).width() > 768) {
-				$("body").scrollTop(0);
-				element.css("height", getHeight());
+.directive('scrollPane', function(Debounce, $timeout) {
+	return {
+		link: function(scope, element, attrs) {
+			function getHeight() {
+				return $(window).height() - $("#mainHeader").height() - element.siblings(".scroll-header").height() + "px";
 			}
-			else {
-				element.css("height", "auto");
+
+			function adjustHeight() {
+				if ($(window).width() > 768) {
+					$("body").scrollTop(0);
+					element.css("height", getHeight());
+				}
+				else {
+					element.css("height", "auto");
+				}
 			}
-		}));
+
+			adjustHeight();
+	 
+	 		$(window).resize(Debounce(adjustHeight));
+
+			scope.$watch(attrs.watch, function() {
+				$timeout(adjustHeight);
+			});
+		}
 	}
 });
