50cc209a9327af5b17af898638153ed98eb795bb
Gabriel Masters-Jackson <gabriel.mj2@gmail.com>
2013-11-05 16:31:09 -0500

    adding main form validation, focusMe directive, fixing button styles for use with form validation

--- www/modules/calendar/taskCtl.js
+++ www/modules/calendar/taskCtl.js
@@ -2,18 +2,21 @@
 
 angular.module('pinwheelApp')
   .controller('TaskCtl', function ($scope, Calendar, Task, ReminderService, $filter) {
-  		//create new task for later use
-		$scope.newTask = new Task({
-			has_reminder: false,
-			has_due_date: false
-		});
-
-		$scope.useReminderType = '';
+		$scope.defaultTask = function() {
+			return {
+				has_reminder: false,
+				has_due_date: false
+			};
+		}
 
   		//scope toggle function
 		$scope.toggle = function(name) {
 			$scope[name] = !$scope[name];
 		}
+
+		//create new task for later use
+		$scope.newTask = new Task($scope.defaultTask());
+		$scope.useReminderType = '';
 
 		//open form for new task adding
 		$scope.add = function() {
@@ -22,7 +25,7 @@ angular.module('pinwheelApp')
 		}
 		
 		//save new task
-		$scope.save = function(){
+		$scope.save = function() {
 			$scope.newTask.$save({}, function(task) {
 				$scope.events.push(task);
 				$scope.cancel();
@@ -37,54 +40,57 @@ angular.module('pinwheelApp')
 
 		//reset new task and form
 		$scope.reset = function() {
-			$scope.newTask = new Task();
+			$scope.newTask = new Task($scope.defaultTask());
 			$scope.newTask.calendar_id = $scope.user.settings.default_calendar;
-		}
-
-		$scope.dueDateToggle = function() {
-			if ($scope.newTask.has_due_date) {
-				$scope.useReminderType = 'relative';
-				$scope.newTask.due_time = new Date($filter('date')((new Date()).addHours(1), "M/d/yyyy h:00 a"));
-				(!$scope.newTask.has_reminder && $scope.checkCalendarReminder());
-			}
-			else {
-				$scope.useReminderType = '';
-				$scope.newTask.due_time = 0;
-				($scope.newTask.has_reminder && ReminderService.reminderDefaultsTask($scope.newTask, $scope.user));
-			}
+			$scope.useReminderType = '';
 		}
 
 		$scope.reminderTypeFilter = function(reminderType) {
 			return (reminderType.type==$scope.useReminderType || reminderType.type=='both');
 		}
 
-		$scope.reminderToggle = function() {
+		$scope.$watch("newTask.has_due_date", function(newVal) {
+			$scope.useReminderType = (newVal) ? "relative" : "";
+		});
+
+		$scope.dueDateToggle = function(task) {
+			if (task.has_due_date) {
+				task.start = new Date($filter('date')((new Date()).addHours(1), "M/d/yyyy h:00 a"));
+				(!task.has_reminder && $scope.checkCalendarReminder(task));
+			}
+			else {
+				task.start = 0;
+				(task.has_reminder && ReminderService.reminderDefaultsTask(task, $scope.user));
+			}
+		}
+
+		$scope.reminderToggle = function(task) {
 			//if adding reminder
-			if ($scope.newTask.has_reminder) {
+			if (task.has_reminder) {
 				//if no existing reminder (calendar or other), use default reminder settings
-				($scope.newTask.reminder_pref_id == null && ReminderService.reminderDefaultsTask($scope.newTask, $scope.user));
+				(task.reminder_pref_id == null && ReminderService.reminderDefaultsTask(task, $scope.user));
 			}
 			//if removing reminder
 			else {
-				$scope.checkCalendarReminder();
+				(task.has_due_date && $scope.checkCalendarReminder(task));
 			}
 		}
 
-		$scope.switchCalendar = function() {
-			if ((!$scope.newTask.has_reminder || $scope.newTask.using_calendar_reminder) && $scope.newTask.has_due_date) {
-				$scope.checkCalendarReminder();
+		$scope.switchCalendar = function(task) {
+			if ((!task.has_reminder || task.using_calendar_reminder) && task.has_due_date) {
+				$scope.checkCalendarReminder(task);
 			}
 		}
 
-		$scope.checkCalendarReminder = function() {
-			ReminderService.checkCalendarReminder($scope.newTask, $scope.calendarWatchers);
+		$scope.checkCalendarReminder = function(task) {
+			ReminderService.checkCalendarReminder(task, $scope.calendarWatchers);
 		}
 
-		$scope.usingOwnReminder = function() {
-			ReminderService.usingOwnReminder($scope.newTask);
+		$scope.usingOwnReminder = function(task) {
+			ReminderService.usingOwnReminder(task);
 		}
 
-		$scope.noReminder = function() {
-			ReminderService.noReminder($scope.newTask);
+		$scope.noReminder = function(task) {
+			ReminderService.noReminder(task);
 		}
   });
--- www/modules/calendar/viewTaskDirective/viewTask.js
+++ www/modules/calendar/viewTaskDirective/viewTask.js
@@ -1,97 +1,62 @@
 'use strict';
 
 angular.module('pinwheelApp')
-	.directive('viewTask', function($filter, ReminderService){
-		return {
-			restrict: 'E',
-			templateUrl: 'modules/calendar/viewTaskDirective/_view_task.html',
-			scope: {
-				task: '=',
-				calendars: '=',
-				calendarWatchers: '=',
-				reminderTypes: '='
-			},
-			controller: function($scope, $element, $attrs, $routeParams){
-				$scope.task.has_due_date = $scope.task.hasOwnProperty("start");
-				$scope.useReminderType = '';
-				$scope.edit = function() {
-					$scope.editTask || ($scope.editTask = {});
-					angular.copy($scope.task, $scope.editTask);
-					$scope.useReminderType = ($scope.editTask.has_due_date) ? 'relative' : '';
+.directive('viewTask', function($filter, ReminderService){
+	return {
+		restrict: 'E',
+		templateUrl: 'modules/calendar/viewTaskDirective/_view_task.html',
+		scope: {
+			task: '=',
+			calendars: '=',
+			calendarWatchers: '=',
+			reminderTypes: '=',
+			reminderToggle: '=',
+			switchCalendar: '=',
+			checkCalendarReminder: '=',
+			dueDateToggle: '=',
+			usingOwnReminder: '=',
+			noReminder: "="
+		},
+		controller: function($scope, $element, $attrs, $routeParams){
+			$scope.task.has_due_date = $scope.task.hasOwnProperty("start");
+			$scope.useReminderType = '';
+			$scope.edit = function() {
+				$scope.editTask || ($scope.editTask = {});
+				angular.copy($scope.task, $scope.editTask);
+				$scope.useReminderType = ($scope.editTask.has_due_date) ? 'relative' : '';
 
-					//apply calendar reminder if task doesn't have one already AND has a due date
-					(!$scope.editTask.has_reminder &&
-					$scope.editTask.has_due_date &&
-					$scope.checkCalendarReminder());
+				//apply calendar reminder if task doesn't have one already AND has a due date
+				(!$scope.editTask.has_reminder &&
+				$scope.editTask.has_due_date &&
+				$scope.checkCalendarReminder());
 
-					$scope.editingTask = true;
-				}
-
-				$scope.cancel = function() {
-					$scope.editingTask = false;
-				}
-
-				$scope.update = function() {
-					angular.copy($scope.editTask, $scope.task);
-					$scope.task.$update({id: $scope.task.id}, function(task){
-						$scope.task = task;
-						$scope.cancel();
-					});
-				}
-
-				$scope.delete = function() {
-					$scope.task.$delete({id: $scope.task.id, version: $scope.task.version});
-					$scope.cancel();
-				}
-
-				$scope.dueDateToggle = function() {
-					if ($scope.editTask.has_due_date) {
-						$scope.useReminderType = 'relative';
-						$scope.editTask.start = new Date($filter('date')((new Date()).addHours(1), "M/d/yyyy h:00 a"));
-						(!$scope.editTask.has_reminder && ReminderService.checkCalendarReminder($scope.editTask, $scope.calendarWatchers));
-					}
-					else {
-						$scope.useReminderType = '';
-						$scope.editTask.start = 0;
-						ReminderService.usingOwnReminder($scope.editTask);
-						($scope.editTask.has_reminder && ReminderService.reminderDefaultsTask($scope.editTask, $scope.user));
-					}
-				}
-
-				$scope.reminderTypeFilter = function(reminderType) {
-					return (reminderType.type==$scope.useReminderType || reminderType.type=='both');
-				}
-
-
-				$scope.reminderToggle = function() {
-					//if adding reminder
-					if ($scope.editTask.has_reminder) {
-						//if no existing reminder (calendar or other), use default reminder settings
-						($scope.editTask.reminder_pref_id == null && ReminderService.reminderDefaultsTask($scope.editTask, $scope.user));
-					}
-					//if removing reminder
-					else {
-						$scope.checkCalendarReminder();
-					}
-				}
-
-				$scope.switchCalendar = function() {
-					if ((!$scope.editTask.has_reminder || $scope.editTask.using_calendar_reminder) && $scope.editTask.has_due_date) {
-						$scope.checkCalendarReminder();
-					}
-				}
-
-				$scope.checkCalendarReminder = function() {
-					ReminderService.checkCalendarReminder($scope.editTask, $scope.calendarWatchers);
-				}
-
-				$scope.usingOwnReminder = function() {
-					ReminderService.usingOwnReminder($scope.editTask);
-				}
-
-				$scope.noReminder = function() {
-					ReminderService.noReminder($scope.editTask);
-				}
+				$scope.editingTask = true;
 			}
+
+			$scope.cancel = function() {
+				$scope.editingTask = false;
+			}
+
+			$scope.update = function() {
+				angular.copy($scope.editTask, $scope.task);
+				$scope.task.$update({id: $scope.task.id}, function(task){
+					$scope.task = task;
+					$scope.cancel();
+				});
+			}
+
+			$scope.delete = function() {
+				$scope.task.$delete({id: $scope.task.id, version: $scope.task.version});
+				$scope.cancel();
+			}
+
+			$scope.reminderTypeFilter = function(reminderType) {
+				return (reminderType.type==$scope.useReminderType || reminderType.type=='both');
+			}
+
+			$scope.$watch("editTask.has_due_date", function(newVal) {
+				$scope.useReminderType = (newVal) ? "relative" : "";
+			});
 		}
-	});
+	}
+});
