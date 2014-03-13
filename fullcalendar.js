ad28a01c4a873777c3849121c13d5f566d7f6ac2
Gabriel Masters-Jackson <gabriel.mj2@gmail.com>
2014-03-13 15:12:33 -0400

    now only moving events during a drag if not repeating

--- fullcalendar.js
+++ fullcalendar.js
@@ -6196,7 +6196,8 @@ function View(element, calendar, viewName) {
 	function eventDrop(e, event, dayDelta, minuteDelta, allDay, ev, ui) {
 		var oldAllDay = event.allDay;
 		var eventId = event._id;
-		moveEvents(eventsByID[eventId], dayDelta, minuteDelta, allDay);
+		console.log(event);
+		if (!event.initiallyRepeating) moveEvents(eventsByID[eventId], dayDelta, minuteDelta, allDay);
 		trigger(
 			'eventDrop',
 			e,
